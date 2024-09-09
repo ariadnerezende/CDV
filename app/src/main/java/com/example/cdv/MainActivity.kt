4690a7307b3fd51e0ada3e23aed5115ea03751a4
Ariadne <ariadnederezende@gmail.com>
2024-09-09 20:32:04 -0300

    onSalve and onRestore

--- app/src/main/java/com/example/cdv/MainActivity.kt
+++ app/src/main/java/com/example/cdv/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.cdv
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,9 +17,24 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    companion object{
+        const val VALOR_TELEFONE_UM = "VALOR_TELEFONE_UM"
+        const val VALOR_TELEFONE_DOIS = "VALOR_TELEFONE_DOIS"
+    }
+
+    private val telefoneDoisEt: EditText by lazy{
+        EditText(this)
+    }
+
+    //onCreate pode receber um Bundle (parametro) nulo, exceto se o onSave for chamado anteriormente!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        telefoneDoisEt.hint = "Segundo telefone"
+        telefoneDoisEt.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        amb.root.addView(telefoneDoisEt)
 
         amb.abraBt.setOnClickListener {
             val anotherIntent: Intent = Intent(this, AnotherActivity::class.java).also{
@@ -25,6 +44,27 @@ class MainActivity : AppCompatActivity() {
 
         Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo de vida completo.") //Inicia
         }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VALOR_TELEFONE_UM, amb.telefoneUmEt.text.toString())
+        outState.putString(VALOR_TELEFONE_DOIS, telefoneDoisEt.text.toString())
+        Log.v((getString(R.string.app_name)), "onSaveInstanceState: Salvando dados de instância.")
+    }
+
+    //onRestore NÃO pode receber um Bundle (parametro) nulo!
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.apply {
+            getString(VALOR_TELEFONE_UM)?.let {
+                amb.telefoneUmEt.setText(it)
+            }
+            getString(VALOR_TELEFONE_DOIS, "00000000").let {
+                telefoneDoisEt.setText(it)
+            }
+        }
+        Log.v((getString(R.string.app_name)), "onRestoreInstanceState: Restaurando dados de instância.")
+    }
 
     override fun onStart() {
         super.onStart()
